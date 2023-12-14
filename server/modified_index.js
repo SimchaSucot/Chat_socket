fbe885f64cf80ad2d304226099732ee9a6bb1bb8
Simcha Sucot <simchasucot@gmail.com>
2023-12-15 01:27:37 +0200

    update

--- server/modified_index.js
+++ server/modified_index.js
@@ -6,7 +6,6 @@ import cors from "cors";
 import mongoose from "mongoose";
 import { ConnectedUser, DbMessage, StorMessage } from "./models";
 
-const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 const app = express();
 const server = createServer(app);
