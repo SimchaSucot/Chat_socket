eea6b82eddb398c5fb837755ecd1b81f0f046cc1
Simcha Sucot <simchasucot@gmail.com>
2023-12-15 02:28:12 +0200

    update

--- server/corrected_index.js
+++ server/corrected_index.js
@@ -48,96 +48,97 @@ function getMessage(sender, recipientName, message) {
 io.on("connection", (socket) => {
   // Handle user joining
 
-socket.on("join", async (username) => {
-  try {
-    console.log(`${socket.id} joined with username: ${username}`);
-    // Example of using async/await with Mongoose model
-    // This should be adapted to your specific logic
-    let user = await ConnectedUser.findOne({ username: username });
-    if (!user) {
-      user = await ConnectedUser.create({ username: username });
+  socket.on("join", async (username) => {
+    try {
+      console.log(`${socket.id} joined with username: ${username}`);
+      // Example of using async/await with Mongoose model
+      // This should be adapted to your specific logic
+      let user = await ConnectedUser.findOne({ username: username });
+      if (!user) {
+        user = await ConnectedUser.create({ username: username });
+      }
+      // Add more logic here as per your application's requirements
+    } catch (error) {
+      console.error("Error in join event:", error);
     }
-    // Add more logic here as per your application's requirements
-  } catch (error) {
-    console.error("Error in join event:", error);
-  }
-  // });
-  connectedUsers.set(username, socket.id);
+    connectedUsers.set(username, socket.id);
 
-  if (dbMessages.has(username)) {
-    const dbUser = dbMessages.get(username);
-    const listChats = Array.from(dbUser.keys());
-    io.to(socket.id).emit("list chats", listChats);
-    socket.join(username);
-    dbUser.forEach((array, key) => {
-      array.forEach((obj) => {
-        Object.entries(obj).forEach(([sndr, msg]) => {
-          let senderName = sndr === "me" ? "you" : key;
-          io.to(socket.id).emit("get private message", {
-            senderName: senderName,
-            message: msg,
+    if (dbMessages.has(username)) {
+      const dbUser = await dbMessages.get(username);
+      const listChats = Array.from(dbUser.keys());
+      io.to(socket.id).emit("list chats", listChats);
+      socket.join(username);
+      dbUser.forEach((array, key) => {
+        array.forEach((obj) => {
+          Object.entries(obj).forEach(([sndr, msg]) => {
+            let senderName = sndr === "me" ? "you" : key;
+            io.to(socket.id).emit("get private message", {
+              senderName: senderName,
+              message: msg,
+            });
           });
         });
       });
-    });
-  } else {
-    dbMessages.set(username, new Map());
-  }
-  console.log(dbMessages, "test 1 db"); ////////////////////////////////////////////////////
-
-  if (storMessages.has(username)) {
-    let stor = storMessages.get(username);
-    for (let msg of stor) {
-      getMessage(msg.sender, username, msg.message);
+    } else {
+      await dbMessages.set(username, new Map());
     }
-    storMessages.delete(username);
-  }
-});
+    console.log(dbMessages, "test 1 db"); ////////////////////////////////////////////////////
 
-// Handle private message sending
-socket.on("send private message", ({ recipientName, message }) => {
-  const sender = getKeyByValue(connectedUsers, socket.id);
-  const recipientSocketId = connectedUsers.get(recipientName);
-
-  console.log(`from ${sender} to ${recipientName}: ${message}`); ////////////////////////////
-
-  // Update sender's messages
-  if (!dbMessages.has(sender)) {
-    dbMessages.set(sender, new Map());
-  }
-
-  const dbSender = dbMessages.get(sender);
-  if (!dbSender.has(recipientName)) {
-    dbSender.set(recipientName, []);
-  }
-  dbSender.get(recipientName).push({ me: message });
-
-  // Send the message to the recipient if online
-  if (recipientSocketId) {
-    getMessage(sender, recipientName, message);
-  }
-  // If recipient is offline, store the message to send later
-  else {
-    if (!storMessages.has(recipientName)) {
-      storMessages.set(recipientName, []);
+    if (await storMessages.has(username)) {
+      let stor = await storMessages.get(username);
+      for (let msg of stor) {
+        getMessage(msg.sender, username, msg.message);
+      }
+      await storMessages.delete(username);
     }
-    storMessages.get(recipientName).push({
-      sender: sender,
-      message: message,
-    });
-  }
-  console.log(storMessages, "test 2 stor"); //////////////////////////////////////////////////
-});
+  });
 
-// Handle disconnection
-socket.on("disconnect", () => {
-  const disconnectedUser = getKeyByValue(connectedUsers, socket.id);
-  connectedUsers.delete(disconnectedUser);
-  if (disconnectedUser) {
-    console.log(`${socket.id} disconnected. Removed user: ${disconnectedUser}`);
-    io.emit("user disconnected", disconnectedUser);
-  }
-});
+  // Handle private message sending
+  socket.on("send private message", async({ recipientName, message }) => {
+    const sender = getKeyByValue(connectedUsers, socket.id);
+    const recipientSocketId = connectedUsers.get(recipientName);
+
+    console.log(`from ${sender} to ${recipientName}: ${message}`); ////////////////////////////
+
+    // Update sender's messages
+    if (!(await dbMessages.has(sender))) {
+      await dbMessages.set(sender, new Map());
+    }
+
+    const dbSender = dbMessages.get(sender);
+    if (!(await dbSender.has(recipientName))) {
+      await dbSender.set(recipientName, []);
+    }
+    await dbSender.get(recipientName).push({ me: message });
+
+    // Send the message to the recipient if online
+    if (recipientSocketId) {
+      getMessage(sender, recipientName, message);
+    }
+    // If recipient is offline, store the message to send later
+    else {
+      if (!(await storMessages.has(recipientName))) {
+        await storMessages.set(recipientName, []);
+      }
+      await storMessages.get(recipientName).push({
+        sender: sender,
+        message: message,
+      });
+    }
+    console.log(storMessages, "test 2 stor"); //////////////////////////////////////////////////
+  });
+
+  // Handle disconnection
+  socket.on("disconnect", () => {
+    const disconnectedUser = getKeyByValue(connectedUsers, socket.id);
+    connectedUsers.delete(disconnectedUser);
+    if (disconnectedUser) {
+      console.log(
+        `${socket.id} disconnected. Removed user: ${disconnectedUser}`
+      );
+      io.emit("user disconnected", disconnectedUser);
+    }
+  });
 });
 
 // Utility function to get a key from a Map by its value
